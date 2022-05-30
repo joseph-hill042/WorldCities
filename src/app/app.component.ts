e87d3a0494a14e9da09919b37326476a4b2474c2
Joseph Hill <joseph.hill1@outlook.com>
2022-05-30 15:02:06 -0400

    Added new components, routing, and material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public forecasts?: WeatherForecast[];
 
-  constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-  }
+  constructor() { }
 
   title = 'WorldCities';
-}
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
 }
